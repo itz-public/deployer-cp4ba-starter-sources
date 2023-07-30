6916d0d52ac46e090c5a13488d20d9c7c93483c4
Craig Cooper <Craig.Cooper@ibm.com>
2023-07-29 21:14:06 -0500

    updates

--- getVersion.sh
+++ getVersion.sh
@@ -2,14 +2,13 @@
 touch cp-index.yaml
 
 curl -s https://raw.githubusercontent.com/IBM/cloud-pak/master/repo/case/ibm-cp-automation/index.yaml > cp-index.yaml
+appVersion="22.0.2"
+version=$(yq eval '.versions | with_entries(select(.value.appVersion == "*'$appVersion'*")) | keys | .[-1]' cp-index.yaml)
 
-version=$(yq eval '.versions | with_entries(select(.value.appVersion == "22.0.2-IF006")) | keys' cp-index.yaml)
 
-
-version=$(echo "$version" | tr -d '-')
 version="${version#"${version%%[![:space:]]*}"}"
 version="${version%"${version##*[![:space:]]}"}"
-echo "$version"
+echo "$version" 
 
 curl -L -O "https://github.com/IBM/cloud-pak/raw/master/repo/case/ibm-cp-automation/$version/ibm-cp-automation-$version.tgz"
 
